e593817b8bde80c892cf72be16ac882b9065200b
Andrew Huynh <a5.t.huynh@gmail.com>
2012-12-22 14:28:13 -0800

    API authentication now checks for correct username & api key combo

--- api_auth.py
+++ api_auth.py
@@ -1,20 +1,36 @@
-from django.conf.urls import url
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 
-from tastypie.http import HttpUnauthorized, HttpForbidden
-from tastypie.resources import ModelResource
+from twofactor.models import UserAPIToken
 
 
 class ApiTokenAuthentication( Authentication ):
     def is_authenticated( self, request, **kwargs ):
-        return True
+        username = request.GET.get( 'user', None )
+        key = request.GET.get( 'key', None )
+
+        # Ensure the correct parameters were passed in
+        if username is None or key is None:
+            return False
+
+        # Check to see if we have this user AND that they API key
+        # matches those that belong to said user
+        user  = User.objects.get( username=username )
+        tokens = UserAPIToken.objects.filter( user=user )
+        if user is None or len( tokens ) == 0:
+            return False
+
+        for token in tokens:
+            if token.key == key:
+                return True
+
+        # No matching key? Deny access.
+        return False
 
     def get_identifier( self, request ):
-        return request.user.username
+        return request.GET.get( 'user' )
 
 
 class ApiTokenAuthorization( Authorization ):
@@ -23,56 +39,3 @@ class ApiTokenAuthorization( Authorization ):
 
     def apply_limits( self, request, object_list ):
         return None
-
-
-class UserResource( ModelResource ):
-    class Meta:
-        queryset = User.objects.all()
-        fields = [ 'username', 'email' ]
-        allowed_methods = [ 'get', 'post' ]
-        resource_name = 'user'
-
-    def override_urls( self ):
-        return [
-            url( r'^(?P<resource_name>%s)/login/$' %
-                    ( self._meta.resource_name ),
-                    self.wrap_view( 'login' ), name='api_login' ),
-            url( r'^(?P<resource_name>%s)/logout/$' %
-                    ( self._meta.resource_name ),
-                    self.wrap_view( 'logout' ), name='api_logout' ) ]
-
-    def login( self, request, **kwargs ):
-        self.method_check( request, allowed=[ 'get' ] )
-
-        data = request.GET
-
-        username = data.get( 'username', '' )
-        password = data.get( 'password', '' )
-        token    = data.get( 'token', '' )
-
-        user = authenticate(username=username, password=password, token=token)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return self.create_response(request, {
-                    'success': True
-                })
-            else:
-                return self.create_response(request, {
-                    'success': False,
-                    'reason': 'disabled',
-                    }, HttpForbidden)
-        else:
-            return self.create_response(request, {
-                'success': False,
-                'reason': 'incorrect',
-                }, HttpUnauthorized)
-
-    def logout(self, request, **kwargs):
-        self.method_check(request, allowed=['get'])
-        if request.user and request.user.is_authenticated():
-            logout(request)
-            return self.create_response(request, { 'success': True })
-        else:
-            return self.create_response(request, { 'success': False },
-                                        HttpUnauthorized)
